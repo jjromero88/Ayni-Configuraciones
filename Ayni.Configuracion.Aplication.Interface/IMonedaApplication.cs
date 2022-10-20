54d707a5ba3088b874f4c9e65aa3f5bff927f578
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 22:54:19 -0500

    Ayni.Configuracion.Aplication.Interface
    Ayni.Configuracion.Aplication.Interface

---
+++ Ayni.Configuracion.Aplication.Interface/IMonedaApplication.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Ayni.Configuracion.Aplication.Dto;
+using Ayni.Configuracion.Transversal.Common;
+
+namespace Ayni.Configuracion.Aplication.Interface
+{
+    public interface IMonedaApplication
+    {
+
+        #region Metodos Sincronos
+        Response<bool> Insert(MonedaDto monedaDto);
+        Response<bool> Update(MonedaDto monedaDto);
+        Response<bool> Delete(int monedaId);
+        Response<MonedaDto> Get(int monedaId);
+        Response<IEnumerable<MonedaDto>> GetAll();
+        #endregion
+
+
+        #region Metodos Asincronos
+        Task<Response<bool>> InsertAsync(MonedaDto monedaDto);
+        Task<Response<bool>> UpdateAsync(MonedaDto monedaDto);
+        Task<Response<bool>> DeleteAsync(int monedaId);
+        Task<Response<MonedaDto>> GetAsync(int monedaId);
+        Task<Response<IEnumerable<MonedaDto>>> GetAllAsync();
+        #endregion
+
+    }
+}
