217ce7136cfd889794f2b11894bd1de71569ff2e
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 22:53:24 -0500

    Ayni.Configuracion.Aplication.Dto
    Ayni.Configuracion.Aplication.Dto

---
+++ Ayni.Configuracion.Aplication.Dto/MonedaDto.cs
@@ -0,0 +1,18 @@
+﻿namespace Ayni.Configuracion.Aplication.Dto
+{
+    /*
+     * Atributos que seran expuestos
+     */
+    public class MonedaDto
+    {
+        public int moneda_id { get; set; }
+        public string descripcion { get; set; }
+        public string abreviatura { get; set; }
+        public string simbolo { get; set; }
+        public bool estado { get; set; }
+        public string usuario_reg { get; set; }
+        public DateTime? fecha_reg { get; set; }
+        public string usuario_act { get; set; }
+        public DateTime? fecha_act { get; set; }
+    }
+}
