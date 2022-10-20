2a89b6238df33f547f67e332845c45892d728f6c
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 22:57:47 -0500

    Ayni.Configuracion.Domain.Entity
    Ayni.Configuracion.Domain.Entity

---
+++ Ayni.Configuracion.Domain.Entity/Moneda.cs
@@ -0,0 +1,15 @@
+﻿namespace Ayni.Configuracion.Domain.Entity
+{
+    public class Moneda
+    {
+        public int moneda_id { get; set; }
+        public string descripcion { get; set; }
+        public string abreviatura { get; set; }
+        public string simbolo { get; set; }
+        public bool estado { get; set; }
+        public string usuario_reg { get; set; }
+        public DateTime? fecha_reg { get; set; }
+        public string usuario_act { get; set; }
+        public DateTime? fecha_act { get; set; }
+    }
+}
