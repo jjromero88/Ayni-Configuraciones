de7a0ae6fe1b2bda7908eef37d752a5326df088d
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 23:02:06 -0500

    Ayni.Configuracion.Transversal.Common
    Ayni.Configuracion.Transversal.Common

---
+++ Ayni.Configuracion.Transversal.Common/IConnectionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Data;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayni.Configuracion.Transversal.Common
+{
+    public interface IConnectionFactory
+    {
+        IDbConnection GetConnection { get; }
+    }
+}
---
+++ Ayni.Configuracion.Transversal.Common/Response.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ayni.Configuracion.Transversal.Common
+{
+    public class Response<T>
+    {
+        public T Data { get; set; }
+        public bool IsSuccess { get; set; }
+        public string Message { get; set; }
+    }
+}
