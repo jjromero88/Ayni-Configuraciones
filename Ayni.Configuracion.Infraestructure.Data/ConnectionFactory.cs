ccec59f5474fc977350e7d283c30ec21e482830e
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 22:59:24 -0500

    Ayni.Configuracion.Infraestructure.Data
    Ayni.Configuracion.Infraestructure.Data

---
+++ Ayni.Configuracion.Infraestructure.Data/ConnectionFactory.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Ayni.Configuracion.Transversal.Common;
+using Microsoft.Extensions.Configuration;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace Ayni.Configuracion.Infraestructure.Data
+{
+
+    /*
+     * Responsabilidad:
+     * Conectarse a la BD y devolver la instancia de la coneccion
+     */
+
+    public class ConnectionFactory : IConnectionFactory
+    {
+
+        private readonly IConfiguration _configuration;
+
+        public ConnectionFactory(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        /*
+         Accede a la bd y devuelve una instancia de coneccion abierta
+         */
+        public IDbConnection GetConnection
+        {
+            get
+            {
+                var sqlConnection = new SqlConnection();
+                if (sqlConnection == null) return null;
+
+                sqlConnection.ConnectionString = _configuration.GetConnectionString("AyniConfiguracionConnection");
+                sqlConnection.Open();
+                return sqlConnection;
+            }
+        }
+    }
+}
