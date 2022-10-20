9a47361b0c2f90b77c5ebe306407e207c5540b73
jjromero88 <67927004+jjromero88@users.noreply.github.com>
2022-10-19 22:58:45 -0500

    Ayni.Configuracion.Domain.Interface
    Ayni.Configuracion.Domain.Interface

---
+++ Ayni.Configuracion.Domain.Interface/IMonedaDomain.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Ayni.Configuracion.Domain.Entity;
+using System.Threading.Tasks;
+
+namespace Ayni.Configuracion.Domain.Interface
+{
+    public interface IMonedaDomain
+    {
+        #region Metodos Sincronos
+        bool Insert(Moneda moneda);
+        bool Update(Moneda moneda);
+        bool Delete(int moneda_id);
+        Moneda Get(int moneda_id);
+        IEnumerable<Moneda> GetAll();
+        #endregion
+
+
+        #region Metodos Asincronos
+        Task<bool> InsertAsync(Moneda moneda);
+        Task<bool> UpdateAsync(Moneda moneda);
+        Task<bool> DeleteAsync(int moneda_id);
+        Task<Moneda> GetAsync(int moneda_id);
+        Task<IEnumerable<Moneda>> GetAllAsync();
+        #endregion
+    }
+}
